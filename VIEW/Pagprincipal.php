aa8b4ae8a40d79a3c157e4328838598c50cbac27
Dorx-tos <139508704+Dorx-tos@users.noreply.github.com>
2023-11-24 17:29:38 -0500

    Pagprincipal.php

--- VIEW/Pagprincipal.php
+++ VIEW/Pagprincipal.php
@@ -1,28 +1,3 @@
-<?php
-require_once '../CONTROLLER/busqueda.php'; // Incluye el archivo de conexión a la base de datos
-
-// Verificar si se ha enviado el formulario
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Obtener los valores del formulario
-    $nombre = $_POST['buscar'];
-    $buscarpublicacion = $_POST['buscapublicacion'];
-    $preciodesde = $_POST['preciodesde'];
-    $preciohasta = $_POST['preciohasta'];
-    $fecha = $_POST['fecha'];
-
-    // Insertar los valores en la base de datos
-    $resultado = insertarBusqueda($nombre, $buscarpublicacion, $preciodesde, $preciohasta, $fecha);
-
-    if ($resultado) {
-        // Éxito al insertar los datos
-        header('Location: ../VIEW/Pagprincipal.php');
-        exit();
-    } else {
-        // Error al insertar los datos, manejarlo adecuadamente
-        echo "Error al realizar la búsqueda";
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -64,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         if (opcionSeleccionada === "casa") {
             window.location.href = "../MODEL/iniciosesion1.php";
         } else if (opcionSeleccionada === "apartamento") {
-            window.location.href = "../VIEW/compras.php";
+            window.location.href = "../VIEW/compra.html";
         } else if (opcionSeleccionada === "local") {
             window.location.href = "pagina_locales.html";
         }
@@ -72,114 +47,56 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </script>
 </head>
 <body>
-<form id="" method="POST" action="../VIEW/Pagprincipal.php" class="contenedor">
-<div class="subcontenedor">
-<div class="subcontenedor1">
-<label class="form-label">Nombre de vendedor</label>
-<input type="text" class="buscaespecifico" id="buscar" name="buscar" value="<?php echo isset($_POST["buscar"]) ? $_POST["buscar"] : ''; ?>">
-
-
-<div class="col-11">
-<table class="table">
-<thead>
-<tr class="filters">
-Buscar publicaciones
-<select id="assigned-tutor-filter" id="buscapublicacion" name="buscapublicacion" class="form-control mt-2">
-<?php if ($_POST["buscapublicacion"] != ''){?>
-<option value="<?php echo $_POST["buscapublicacion"]; ?>"><?php echo $_POST["buscapublicacion"]; ?></option>
-<?php } ?>
-<option value="Todos">Todos</option>
-<option value="Compras" >Compras</option>
-<option value="Ventas">Ventas</option>
-<option value="Arriendos">Arriendos</option>
-</select>
-Precio desde:
-<input type="text" id="preciodesde" name="preciodesde" class="form-control mt-2" value="<?php echo isset($_POST["preciodesde"]) ? $_POST["preciodesde"] : ''; ?>">
-Precio hasta:
-<input type="text" id="preciohasta" name="preciohasta" class="form-control mt-2" value="<?php echo isset($_POST["preciohasta"]) ? $_POST["preciohasta"] : ''; ?>">
-<label for="fecha">Fecha:</label>
-<input type="date" id="fecha" name="fecha" class="form-control mt-2">
-
-
+<div class="contenedor">
+    <div class="subcontenedor1">
+        <div class="filtros">
+            <h2 class="label1"><br>Filtrado por:</h2>
+            <div class="busqueda">
+            <label class="label1">Buscar:</label>
+            <br>
+                <input class="search" name="text" placeholder="Busqueda..." type="search" >
+            </div>
+            <hr class="hr"/>
+            <div class="precio">
+            <label class="label1">Filtro de busqueda:</label>
+            <br>
+                <input type="text" id="precio_desde" class="busqueda1" placeholder="Precio desde (COP)" value/>
+            </div>
+            <hr class="hr"/>
+            <div class="habitaciones">
+                <select id="habitaciones" class="busqueda2">
+                <option disabled selected value>Habitaciones</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5+">5+</option>
+                </select>
+                </div>
+            <div class="banos">
+                <select id="banos" class="busqueda3">
+                    <option disabled selected value>Baños</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5+">5+</option>
+                </select>
+            </div>
+            <div class="m2">
+                <select id="m2" class="busqueda3">
+                    <option disabled selected value>m2</option>
+                    <option value="1">5m2</option>
+                    <option value="2">50m2</option>
+                    <option value="3">100m2</option>
+                    <option value="4">150m2</option>
+                    <option value="5+">200m2+</option>
+                </select>
+            </div>
             <button class="dirigir"><a href="" class="dirigir1">Aceptar</a></button>
             </div>
     </div>
-    <?php
-$db = new mysqli('localhost', 'root', '', 'real_state');
 
-if ($db->connect_error) {
-  die('Connection failed: ' . $db->connect_error);
-}
-
-$sql = 'SELECT * FROM busqueda';
-
-if(isset($_POST["buscar"]) && $_POST["buscar"] != '') {
-  $sql .= " WHERE nombre = '{$_POST["buscar"]}'"; 
-}
-
-if(isset($_POST["buscarpublicacion"]) && $_POST["buscarpublicacion"] != 'todos') {
-  if(strpos($sql, 'WHERE') === false) {
-    $sql .= " WHERE "; 
-  } else {
-    $sql .= " AND ";
-  }
-  
-  $sql .= "buscarpublicacion = '{$_POST["buscarpublicacion"]}'";
-}
-
-// Añadir otros filtros aquí con la misma lógica...
-
-$result = $db->query($sql);
-
-if(!$result) {
-  die('Query failed: ' . $db->error);
-}
-?>
-<?php
-$result->free();
-$db->close();
-?>
-
-<?php
-    // Obtener los datos de búsqueda y mostrarlos en una tabla
-    $resultados = buscarDatos($_POST);
-
-    if ($resultados && $resultados->num_rows > 0) {
-        ?>
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr style="background-color: #00695c; color:#FFFFFF;">
-                        <th style="text-align: center;"> Nombre </th>
-                        <th style="text-align: center;"> Precio desde </th>
-                        <th style="text-align: center;"> Precio hasta </th>
-                        <th style="text-align: center;"> Fecha </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    while($row = $resultados->fetch_assoc()) {
-                    ?>
-                    <tr>
-                        <td style="text-align: center;"><?php echo $row['nombre']; ?></td>
-                        <td style="text-align: center;"><?php echo $row['preciodesde']; ?></td> 
-                        <td style="text-align: center;"><?php echo $row['preciohasta']; ?></td>
-                        <td style="text-align: center;"><?php echo $row['fecha']; ?></td>
-                    </tr>
-                    <?php
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-        <?php
-        $resultados->free(); // Liberar resultados
-    } else {
-        echo "No se encontraron resultados";
-    }
-    ?>
-    </div>
-    </form>
     <div class="subcontenedor2">
         <div class="ventana">
             <img src="https://i.postimg.cc/MHQ87Lny/438430723.jpg" alt="Casa 1" style="width: 250px; float: center;">
@@ -200,7 +117,7 @@ $db->close();
                     <div class="info1">1</div>
                 </div>
             </div>
-            <button class="comprar"><a href="../VIEW/compras.php" class="comprar_estilo">Ver Propiedad</a></button>
+            <button class="comprar"><a href="../VIEW/compra.html" class="comprar_estilo">Ver Propiedad</a></button>
         </div>
 
         <div class="ventana2">
@@ -223,7 +140,7 @@ $db->close();
                 </div>
             </div>
 
-            <button class="comprar"><a href="../VIEW/compras.php" class="comprar_estilo">Ver Propiedad</a></button>
+            <button class="comprar"><a href="../VIEW/compra.html" class="comprar_estilo">Ver Propiedad</a></button>
         </div>
 
         <div class="ventana3">
@@ -245,7 +162,7 @@ $db->close();
                     <div class="info1">1</div>
                 </div>
             </div>
-            <button class="comprar"><a href="../VIEW/compras.php" class="comprar_estilo">Ver Propiedad</a></button>
+            <button class="comprar"><a href="../VIEW/compra.html" class="comprar_estilo">Ver Propiedad</a></button>
         </div>
     </div>
 </div>
