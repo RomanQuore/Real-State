3ec2e383b4eff7e27334c6020766c3c42487960b
RomanQuore <139508526+RomanQuore@users.noreply.github.com>
2023-11-24 17:24:00 -0500

    Add files via upload

--- VIEW/Pagprincipal.php
+++ VIEW/Pagprincipal.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="../CSS/style_principal.css">
     <script src="../JS/Principal.js"></script>
     <title> Pagina Principal </title>
     <div class="container">
