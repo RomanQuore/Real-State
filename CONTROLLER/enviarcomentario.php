0b648a2dcaf412f6ebae054d8e093ba8cc5ac718
RomanQuore <139508526+RomanQuore@users.noreply.github.com>
2023-11-27 15:57:17 -0500

    Add files via upload

--- CONTROLLER/enviarcomentario.php
+++ CONTROLLER/enviarcomentario.php
@@ -1,10 +1,9 @@
 <?php
 $nombre = $_POST['nombre'];
 $comentario = $_POST['coment'];
-$Calificacion = $_POST['estrellas']; // Aquí se cambió a 'estrellas'
+$Calificacion = $_POST['estrellas'];
 $conexion = mysqli_connect("localhost", "root", "", "real_state");
 
-// Validación y limpieza de datos
 $nombre = mysqli_real_escape_string($conexion, $nombre);
 $comentario = mysqli_real_escape_string($conexion, $comentario);
 $Calificacion = mysqli_real_escape_string($conexion, $Calificacion);
@@ -12,10 +11,10 @@ $Calificacion = mysqli_real_escape_string($conexion, $Calificacion);
 $resultado = mysqli_query($conexion, "INSERT INTO coment (nombre, coment, Calificacion) VALUES ('$nombre', '$comentario', '$Calificacion')");
 
 if ($resultado) {
-    // Éxito al insertar los datos
-    header('Location: ../VIEW/compras.php');
+    header('Location: ../VIEW/compraVendedor.php');
+    header('Location: ../VIEW/compraComprador.php');
 } else {
-    // Error al insertar los datos, manejarlo adecuadamente (puede ser una redirección a una página de error, por ejemplo)
+    
     echo "Error al enviar el comentario.";
 }
 ?>
