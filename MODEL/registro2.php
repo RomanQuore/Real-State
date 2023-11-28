b702d2aa8203b8619faef1b82fdb4269be7a68e7
RomanQuore <139508526+RomanQuore@users.noreply.github.com>
2023-11-28 13:45:01 -0500

    Add files via upload

--- MODEL/registro2.php
+++ MODEL/registro2.php
@@ -6,20 +6,21 @@
     }
     if (isset($_POST['registrate'])){
        if (
-         strlen($_POST['Usuario']) >=5 &&
-         strlen($_POST['contrasena']) >=1 && 
-         strlen($_POST['nombres']) >=1 &&
-         strlen($_POST['apellidos']) >=1 &&
+         strlen($_POST['Usuario']) >=4 &&
+         strlen($_POST['contrasena']) >=4 && 
+         strlen($_POST['nombres']) >=4 &&
+         strlen($_POST['apellidos']) >=4 &&
          strlen($_POST['fechaNacimiento']) >=1 &&
          strlen($_POST['tipoDocumento']) >=1 &&
-         strlen($_POST['numeroDocumento']) >=1 &&
-         strlen($_POST['direccion']) >=1 &&
-         strlen($_POST['correoElectronico']) >=1 &&
+         strlen($_POST['numeroDocumento']) >=4 &&
+         strlen($_POST['direccion']) >=4 &&
+         strlen($_POST['correoElectronico']) >=4 &&
          strlen($_POST['Rol']) >=1 &&
          strlen($_POST['activo']) >=1 )
             {
                 $usuario = isset($_POST["Usuario"]) ? trim($_POST["Usuario"]) : '';
                 $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : '';
+                $contrasena = hash("sha256", $contrasena);
                 $nombres = isset($_POST["nombres"]) ? trim($_POST["nombres"]) : '';
                 $apellidos = isset($_POST["apellidos"]) ? trim($_POST["apellidos"]) : '';
                 $fechaNacimiento = isset($_POST["fechaNacimiento"]) ? $_POST['fechaNacimiento']: '';
@@ -30,6 +31,7 @@
                 $correoElectronico = isset($_POST["correoElectronico"]) ? trim ($_POST["correoElectronico"]): '';
                 $Rol = isset($_POST["Rol"]) ? trim ($_POST["Rol"]): '';
                 $activo = isset($_POST["activo"]) ? trim ($_POST["activo"]): '';
+                
                 $consulta = "INSERT INTO registro (Usuario, Contrasena, nombres, apellidos, fechaNacimiento, tipoDocumento, numeroDocumento, direccion, numeroTelefono, correoElectronico, Rol, activo) 
                 VALUES ('$usuario','$contrasena','$nombres','$apellidos','$fechaNacimiento','$tipoDocumento','$numeroDocumento','$direccion','$numeroTelefono','$correoElectronico','$Rol','$activo')";
                 $resultado = mysqli_query($conex,$consulta);
