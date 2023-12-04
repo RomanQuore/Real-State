5e440f368509841488b5151021cdabb0a93a475c
RomanQuore <139508526+RomanQuore@users.noreply.github.com>
2023-12-03 23:32:16 -0500

    Delete JS/Principal.js

--- JS/Principal.js
+++
@@ -1,6 +0,0 @@
-const scrollDownButton = document.querySelector('#scrollDownButton');
-
-scrollDownButton.addEventListener('click', () => {
-  const scrollDownOptions = document.querySelector('#scrollDownOptions');
-  scrollDownOptions.style.display = 'block'; /* Show the options on click */
-});
